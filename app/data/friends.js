6848289967767d9fabde1943e7ff64c8b73e5181
Keely Brennan <keelybrennan@Keelys-MBP-2.attlocal.net>
2018-04-22 13:09:14 -0500

    html routes post pathes

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+{
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+
+}
+
+// Star Wars Characters (DATA)
+// =============================================================
+var characters = [
+    {
+      routeName: "yoda",
+      name: "Yoda",
+      role: "Jedi Master",
+      age: 900,
+      forcePoints: 2000
+    },
+    {
+      routeName: "darthmaul",
+      name: "Darth Maul",
+      role: "Sith Lord",
+      age: 200,
+      forcePoints: 1200
+    },
+    {
+      routeName: "obiwankenobi",
+      name: "Obi Wan Kenobi",
+      role: "Jedi Master",
+      age: 55,
+      forcePoints: 1350
+    }
+  ];
+  
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//* A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+app.get("/api/friends", function(req, res) {
+  return res.json(friends);
+});
+
+// Displays a single friend, or returns false
+app.get("/api/friends/:friend", function(req, res) {
+  var chosen = req.params.friend;
+
+  console.log(chosen);
+
+  for (var i = 0; i < friends.length; i++) {
+    if (chosen === friends[i].routeName) {
+      return res.json(friends[i]);
+    }
+  }
+  return res.json(false);
+});
+
+//* A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+app.post("/api/friends", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newFriend = req.body;
+  
+    console.log(newFriend);
+  
+    // We then add the json the user sent to the character array
+    friends.push(newFriend);
+  
+    // We then display the JSON to the users
+    res.json(newFriend);
+});
