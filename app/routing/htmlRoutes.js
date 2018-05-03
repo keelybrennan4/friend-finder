57bdc015a048ad60e485483abf87de86bae7eb43
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-05-03 18:09:38 -0500

    css

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,12 +4,14 @@ module.exports = function (app) {
 
     //when user clicks button to /survey, get survey.html
     app.get('/survey', function(req, res) {
-        res.sendFile(path.join(__dirname + '/../public/survey.html'));
+        res.sendFile( path.join( __dirname, "../public/survey.html" ) );
+
     });
 
     //if url is not survey, get home page
     app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname + '/../public/home.html'));
+        res.sendFile( path.join( __dirname, "../public/home.html" ) );
+
     });
 
 };
--- server.js
+++ server.js
@@ -19,7 +19,8 @@ require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
 //allows html to have access to our public folder on the server
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + 'public'));
+app.use(express.static(path.join(__dirname, './app/public')));
 
 app.listen(PORT, function(){
     console.log("App listening on PORT" + PORT);
