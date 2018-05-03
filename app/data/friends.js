8cf03bb55b11a4ab284147241bdd5ff6f90f508c
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-05-02 21:57:38 -0500

    friends data structure

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,39 +1,124 @@
-//use to link css styling at the top of each sheet
-//app.use(express.static(path.join(__dirname,'public')))
-
 var friends = [ 
   {
   name: "Frank Gallagher",
   photo:"https://vignette.wikia.nocookie.net/shameless/images/a/af/Rr.jpg/revision/latest?cb=20160912192013",
-  scores:[2,5,5,4,1,3,5,1,2,1]
-},{
+  scores:[
+    "2",
+    "5",
+    "5",
+    "4",
+    "1",
+    "3",
+    "5",
+    "1",
+    "2",
+    "1"
+  ]
+  },{
    name: "Fiona Gallagher",
    photo:"https://vignette.wikia.nocookie.net/shameless/images/c/c8/Tumblr_o1h2rvwJ9I1tuehrqo1_1280.jpg/revision/latest?cb=20160125193117",
-   scores :[5,4,2,3,2,4,3,5,1,5]
-},{
-   "name": "Lip Gallagher",
-   "photo":"https://vignette.wikia.nocookie.net/shameless/images/f/f6/LIP-GALLAGHER-CHARACTER-PORTAL.png/revision/latest?cb=20170126213541",
-   "scores":[1,3,3,5,3,5,4,2,5,4]
-},{
-   "name": "Ian Gallagher",
-   "photo":"https://vignette.wikia.nocookie.net/shameless/images/3/3b/Ian.jpg/revision/latest?cb=20120313203248",
-   "scores":[3,1,4,1,5,1,1,3,3,2]
-},{
-   "name": "Debbie Gallagher",
-   "photo":"https://vignette.wikia.nocookie.net/shameless/images/c/c6/Franny_insieme_alla_sua_mamma_--Debbie--.jpg/revision/latest?cb=20180201144802",
-   "scores":[4,2,1,2,4,2,2,4,4,3]
-},{
-  "name": "Carl Gallagher",
-  "photo":"https://vignette.wikia.nocookie.net/shameless/images/4/45/EthanPortBFBedit.jpg/revision/latest?cb=20180409175155",
-  "scores":[4,2,1,2,4,2,2,4,4,3]
-},{
-  "name": "Liam Gallagher",
-  "photo":"http://images6.fanpop.com/image/photos/40700000/Shameless-Season-8-Liam-Gallagher-Portrait-shameless-us-40782267-446-595.jpg",
-  "scores":[4,2,1,2,4,2,2,4,4,3]
-},{
-  "name": "Monica Gallagher",
-  "photo":"https://vignette.wikia.nocookie.net/shameless/images/1/18/Images-12.jpg/revision/latest?cb=20160912112837",
-  "scores":[4,2,1,2,4,2,2,4,4,3]
+   scores:[
+    "2",
+    "5",
+    "5",
+    "4",
+    "1",
+    "3",
+    "5",
+    "1",
+    "2",
+    "1"
+  ]
+  },{
+   name: "Lip Gallagher",
+   photo:"https://vignette.wikia.nocookie.net/shameless/images/f/f6/LIP-GALLAGHER-CHARACTER-PORTAL.png/revision/latest?cb=20170126213541",
+   scores:[
+    "2",
+    "5",
+    "5",
+    "4",
+    "1",
+    "3",
+    "5",
+    "1",
+    "2",
+    "1"
+  ]
+  },{
+   name: "Ian Gallagher",
+   photo:"https://vignette.wikia.nocookie.net/shameless/images/3/3b/Ian.jpg/revision/latest?cb=20120313203248",
+   scores:[
+    "2",
+    "5",
+    "5",
+    "4",
+    "1",
+    "3",
+    "5",
+    "1",
+    "2",
+    "1"
+  ]
+  },{
+   name: "Debbie Gallagher",
+   photo: "https://vignette.wikia.nocookie.net/shameless/images/c/c6/Franny_insieme_alla_sua_mamma_--Debbie--.jpg/revision/latest?cb=20180201144802",
+   scores:[
+    "2",
+    "5",
+    "5",
+    "4",
+    "1",
+    "3",
+    "5",
+    "1",
+    "2",
+    "1"
+  ]
+  },{
+  name: "Carl Gallagher",
+  photo:"https://vignette.wikia.nocookie.net/shameless/images/4/45/EthanPortBFBedit.jpg/revision/latest?cb=20180409175155",
+  scores:[
+    "2",
+    "5",
+    "5",
+    "4",
+    "1",
+    "3",
+    "5",
+    "1",
+    "2",
+    "1"
+  ]
+  },{
+  name: "Liam Gallagher",
+  photo:"http://images6.fanpop.com/image/photos/40700000/Shameless-Season-8-Liam-Gallagher-Portrait-shameless-us-40782267-446-595.jpg",
+  scores:[
+    "2",
+    "5",
+    "5",
+    "4",
+    "1",
+    "3",
+    "5",
+    "1",
+    "2",
+    "1"
+  ]
+  },{
+  name: "Monica Gallagher",
+  photo:"https://vignette.wikia.nocookie.net/shameless/images/1/18/Images-12.jpg/revision/latest?cb=20160912112837",
+  scores:[
+    "2",
+    "5",
+    "5",
+    "4",
+    "1",
+    "3",
+    "5",
+    "1",
+    "2",
+    "1"
+  ]  
 }
 ]
 
